575b9954ab4a91427b13ceb14046a893728a90ed
Karl Bennett <karl@karl-youthnet.(none)>
2010-12-23 17:46:32 +0000

    Added a mapCan method, some private helper methods, and also modified the map methods so that they can now return collections of different types to the collection passed in.

--- src/main/java/org/lambda/util/Collections.java
+++ src/main/java/org/lambda/util/Collections.java
@@ -2,8 +2,15 @@ package org.lambda.util;
 
 import org.lambda.Lambda;
 
-import java.util.Collection;
-import java.util.List;
+import java.beans.beancontext.BeanContext;
+import java.beans.beancontext.BeanContextServices;
+import java.beans.beancontext.BeanContextServicesSupport;
+import java.beans.beancontext.BeanContextSupport;
+import java.util.*;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author Karl Bennett
@@ -13,14 +20,136 @@ public class Collections {
     private Collections() {
     }
 
-    public static <E, C extends Collection<E>> C map(C collection, Lambda<E, E> lambda) {
+    public static <R, E, RC extends Collection<R>, C extends Collection<E>> RC map(C collection, Lambda<R, E> lambda) {
+        // TODO properly handle unwanted null arguments..
+        if (collection == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - collection cannot be null.");
+        if (lambda == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - lambda cannot be null.");
+
+        RC mappedCollection = instantiateCollection((Class<RC>) collection.getClass());
+
+        if (mappedCollection != null) {
+            runOverCollection(collection, lambda, mappedCollection);
+        }
+
+        return mappedCollection;
+    }
+
+    public static <R, E, RC extends Collection<R>, C extends Collection<E>> RC map(Class<RC> returnType, C collection,
+                                                                                   Lambda<R, E> lambda) {
+        // TODO properly handle unwanted null arguments..
+        if (collection == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - collection cannot be null.");
+        if (lambda == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - lambda cannot be null.");
+
+        RC mappedCollection = instantiateCollection(returnType);
+
+        if (mappedCollection != null) {
+            runOverCollection(collection, lambda, mappedCollection);
+        }
+
+        return mappedCollection;
+    }
+
+    public static <R, E, RC extends Collection<R>, C extends Collection<E>> RC map(RC returnCollection, C collection,
+                                                                              Lambda<R, E> lambda) {
+        // TODO properly handle unwanted null arguments..
+        if (returnCollection == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - return collection is null.");
+        if (collection == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - collection is null.");
+        if (lambda == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - lambda is null.");
+
+        runOverCollection(collection, lambda, returnCollection);
+
+        return returnCollection;
+    }
+
+    public static <E, C extends Collection<E>> void mapC(C collection, Lambda<E, E> lambda) {
         if (collection == null) throw new NullPointerException("org.lambda.util.Collections.map - collection is null.");
         if (lambda == null) throw new NullPointerException("org.lambda.util.Collections.map - lambda is null.");
 
-        C mappedCollection = null;
+        runOverCollection(collection, lambda);
+    }
+
+    public static <R, E, RC extends Collection<R>, C extends Collection<Collection<E>>> RC mapCan(Class<RC> returnType,
+                                                                                                  C collection,
+                                                                                                  Lambda<R, E> lambda) {
+        // TODO properly handle unwanted null arguments..
+        if (returnType == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - returnType cannot be null.");
+        if (collection == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - collection cannot be null.");
+        if (lambda == null) throw new NullPointerException(
+                "org.lambda.util.Collections.map - lambda cannot be null.");
+
+        RC mappedCollection = instantiateCollection(returnType);
+
+        if (mappedCollection != null) {
+            runOverDeepCollection(collection, lambda, mappedCollection);
+        }
+
+        return mappedCollection;
+    }
+
+    public static <R, E, RL extends List<R>, L extends List<E>> RL mapList(L list, Lambda<R, L> lambda) {
+        if (list == null) throw new NullPointerException("org.lambda.util.Collections.map - list is null.");
+        if (lambda == null) throw new NullPointerException("org.lambda.util.Collections.map - lambda is null.");
+
+        RL mappedList = instantiateCollection((Class<RL>) list.getClass());
+
+        if (mappedList != null) {
+            runOverList(list, lambda, mappedList);
+        }
+
+        return mappedList;
+    }
+
+
+    private static <E, RC extends Collection<E>> RC instantiateCollection(Class<RC> returnType) {
+        RC newCollection = null;
 
         try {
-            mappedCollection = (C) collection.getClass().newInstance();
+            // Boilerplate code to handle all the default Java collection interfaces. It would be nice if Java had a
+            // default implementation of collection that all the other class used so it could be instantiated and then
+            // cast into the required subclass. Unfortunately AbstractCollection isn't quite enough for what I need.
+            if (returnType.isInterface()) {
+
+                if (BeanContext.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new BeanContextSupport();
+                } else if (BeanContextServices.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new BeanContextServicesSupport();
+                } else if (BlockingDeque.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new LinkedBlockingDeque<E>();
+                } else if (BlockingQueue.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new LinkedBlockingQueue<E>();
+                } else if (Deque.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new ArrayDeque<E>();
+                } else if (List.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new ArrayList<E>();
+                } else if (NavigableSet.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new TreeSet<E>();
+                } else if (Queue.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new PriorityQueue<E>();
+                } else if (Set.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new HashSet<E>();
+                } else if (SortedSet.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new TreeSet<E>();
+                } else if (Collection.class.isAssignableFrom(returnType)) {
+                    newCollection = (RC) new Vector();
+                }
+
+                if (newCollection == null) {
+                    throw new InstantiationException(
+                            "org.lambda.util.Collections.map - does not recognise the collection interface "
+                                    + returnType.getName());
+                }
+            } else {
+                newCollection = returnType.newInstance();
+            }
         } catch (InstantiationException e) {
             // TODO properly handle failed collection instantiation.
             throw new RuntimeException("org.lambda.util.Collections.map - collection type could not be instantiated. "
@@ -31,48 +160,33 @@ public class Collections {
                     + e.getMessage());
         }
 
-        if (mappedCollection != null) {
-            for (E element : collection) {
-                mappedCollection.add(lambda.lambda(element));
-            }
-        }
-
-        return mappedCollection;
+        return newCollection;
     }
 
-    public static <E, C extends Collection<E>> void mapC(C collection, Lambda<E, E> lambda) {
-        if (collection == null) throw new NullPointerException("org.lambda.util.Collections.map - collection is null.");
-        if (lambda == null) throw new NullPointerException("org.lambda.util.Collections.map - lambda is null.");
-
+    private static <R, E> void runOverCollection(Collection<E> collection, Lambda<R, E> lambda) {
         for (E element : collection) {
             lambda.lambda(element);
         }
     }
 
-    public static <E, L extends List<E>> L mapList(L list, Lambda<E, L> lambda) {
-        if (list == null) throw new NullPointerException("org.lambda.util.Collections.map - list is null.");
-        if (lambda == null) throw new NullPointerException("org.lambda.util.Collections.map - lambda is null.");
-
-        L mappedList = null;
-
-        try {
-            mappedList = (L) list.getClass().newInstance();
-        } catch (InstantiationException e) {
-            // TODO properly handle failed list instantiation.
-            throw new RuntimeException("org.lambda.util.Collections.map - list type could not be instantiated. "
-                    + e.getMessage());
-        } catch (IllegalAccessException e) {
-            // TODO properly handle illegal access exception.
-            throw new RuntimeException("org.lambda.util.Collections.map - list type could not be accessed. "
-                    + e.getMessage());
+    private static <R, E> void runOverCollection(Collection<E> collection, Lambda<R, E> lambda,
+                                                 Collection<R> mappedCollection) {
+        for (E element : collection) {
+            mappedCollection.add(lambda.lambda(element));
         }
+    }
 
-        if (mappedList != null) {
-            for (int i = 0; i < list.size(); i++) {
-                mappedList.add(lambda.lambda((L) list.subList(i, list.size())));
-            }
+    private static <R, E> void runOverDeepCollection(Collection<Collection<E>> collection, Lambda<R, E> lambda,
+                                                     Collection<R> mappedCollection) {
+        for (Collection<E> element : collection) {
+            runOverCollection(element, lambda, mappedCollection);
         }
+    }
 
-        return mappedList;
+    private static <R, E, L extends List<E>> void runOverList(List<E> list, Lambda<R, L> lambda,
+                                                              List<R> mappedList) {
+        for (int i = 0; i < list.size(); i++) {
+            mappedList.add(lambda.lambda((L) list.subList(i, list.size())));
+        }
     }
 }
--- src/test/java/org/lambda/util/CollectionsTest.java
+++ src/test/java/org/lambda/util/CollectionsTest.java
@@ -13,11 +13,16 @@ import static org.lambda.util.Collections.*;
  */
 public class CollectionsTest {
 
+    /**
+     * Test map method with collection and lambda.
+     *
+     * @throws Exception
+     */
     @Test
-    public void testMap() throws Exception {
+    public void testMapCL() throws Exception {
         Collection<Integer> collection = new Vector<Integer>(Arrays.asList(1, 2, 3, 4));
 
-        Collection mappedCollection = map(collection, new Lambda<Integer, Integer>() {
+        Collection<Integer> mappedCollection = map(collection, new Lambda<Integer, Integer>() {
             @Override
             public Integer lambda(Integer integer) {
                 return integer + 1;
@@ -33,6 +38,112 @@ public class CollectionsTest {
         assertEquals("mapped elemet 1 correct", new Integer(3), mappedList.get(1));
         assertEquals("mapped elemet 2 correct", new Integer(4), mappedList.get(2));
         assertEquals("mapped elemet 3 correct", new Integer(5), mappedList.get(3));
+    }
+
+    /**
+     * Test map method with collection and lambda with a different return type.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testMapCLDR() throws Exception {
+        Collection<Integer> collection = new Vector<Integer>(Arrays.asList(1, 2, 3, 4));
+
+        Collection<String> mappedCollection = map(collection, new Lambda<String, Integer>() {
+            @Override
+            public String lambda(Integer integer) {
+                return Integer.toString(integer + 1);
+            }
+        });
+
+        assertNotNull("mapped collection created", mappedCollection);
+        assertNotSame("mapped collection different", collection, mappedCollection);
+        assertEquals("mapped collection size correct", collection.size(), mappedCollection.size());
+        List<String> mappedList = new ArrayList<String>(mappedCollection);
+        assertEquals("mapped elemet 0 correct", "2", mappedList.get(0));
+        assertEquals("mapped elemet 1 correct", "3", mappedList.get(1));
+        assertEquals("mapped elemet 2 correct", "4", mappedList.get(2));
+        assertEquals("mapped elemet 3 correct", "5", mappedList.get(3));
+    }
+
+    /**
+     * Test map method with class, collection, and lambda.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testMapClCL() throws Exception {
+        Collection<Integer> collection = new Vector<Integer>(Arrays.asList(1, 2, 3, 4));
+
+        List<Integer> mappedList = map(List.class, collection, new Lambda<Integer, Integer>() {
+            @Override
+            public Integer lambda(Integer integer) {
+                return integer + 1;
+            }
+        });
+
+        assertNotNull("mapped collection created", mappedList);
+        assertFalse("new mapped collection", collection == mappedList);
+        assertNotSame("mapped collection different", collection, mappedList);
+        assertEquals("mapped collection size correct", collection.size(), mappedList.size());
+        assertEquals("mapped elemet 0 correct", new Integer(2), mappedList.get(0));
+        assertEquals("mapped elemet 1 correct", new Integer(3), mappedList.get(1));
+        assertEquals("mapped elemet 2 correct", new Integer(4), mappedList.get(2));
+        assertEquals("mapped elemet 3 correct", new Integer(5), mappedList.get(3));
+    }
+
+    /**
+     * Test map method with return collection, collection, and lambda.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testMapRCCL() throws Exception {
+        Collection<Integer> collection = new Vector<Integer>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> preMappedList = new ArrayList<Integer>();
+
+        List<Integer> postMappedList = map(preMappedList, collection, new Lambda<Integer, Integer>() {
+            @Override
+            public Integer lambda(Integer integer) {
+                return integer + 1;
+            }
+        });
+
+        assertNotNull("mapped collection created", preMappedList);
+        assertFalse("new mapped collection", collection == preMappedList);
+        assertTrue("post list equals pre list", preMappedList == postMappedList);
+        assertNotSame("mapped collection different", collection, preMappedList);
+        assertEquals("mapped collection size correct", collection.size(), preMappedList.size());
+        assertEquals("mapped elemet 0 correct", new Integer(2), preMappedList.get(0));
+        assertEquals("mapped elemet 1 correct", new Integer(3), preMappedList.get(1));
+        assertEquals("mapped elemet 2 correct", new Integer(4), preMappedList.get(2));
+        assertEquals("mapped elemet 3 correct", new Integer(5), preMappedList.get(3));
+    }
+
+    @Test
+    public void testMapCan() throws Exception {
+        Collection<Collection<Integer>> collection = new Vector<Collection<Integer>>();
+        collection.add(new Vector<Integer>(Arrays.asList(1, 2)));
+        collection.add(new Vector<Integer>(Arrays.asList(3, 4)));
+        collection.add(new Vector<Integer>(Arrays.asList(5, 6)));
+
+        List<Integer> mappedList = mapCan(List.class, collection,
+                new Lambda<Integer, Integer>() {
+                    @Override
+                    public Integer lambda(Integer integer) {
+                        return integer + 1;
+                    }
+                });
+
+        assertNotNull("mapped collection created", mappedList);
+        assertNotSame("mapped collection different", collection, mappedList);
+        assertFalse("mapped collection size correct", collection.size() == mappedList.size());
+        assertEquals("mapped elemet 0 correct", new Integer(2), mappedList.get(0));
+        assertEquals("mapped elemet 1 correct", new Integer(3), mappedList.get(1));
+        assertEquals("mapped elemet 2 correct", new Integer(4), mappedList.get(2));
+        assertEquals("mapped elemet 3 correct", new Integer(5), mappedList.get(3));
+        assertEquals("mapped elemet 4 correct", new Integer(6), mappedList.get(4));
+        assertEquals("mapped elemet 5 correct", new Integer(7), mappedList.get(5));
     }
 
     @Test
@@ -56,7 +167,7 @@ public class CollectionsTest {
     public void testMapList() throws Exception {
         List<Integer> list = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
 
-        List mappedList = mapList(list, new Lambda<Integer, List<Integer>>() {
+        List<Integer> mappedList = mapList(list, new Lambda<Integer, List<Integer>>() {
             @Override
             public Integer lambda(List<Integer> tail) {
                 int sum = 0;
@@ -77,5 +188,35 @@ public class CollectionsTest {
         assertEquals("mapped elemet 1 correct", new Integer(9), mappedList.get(1));
         assertEquals("mapped elemet 2 correct", new Integer(7), mappedList.get(2));
         assertEquals("mapped elemet 3 correct", new Integer(4), mappedList.get(3));
+    }
+
+    /**
+     * Map list with different return type.
+     * @throws Exception
+     */
+    @Test
+    public void testMapListDR() throws Exception {
+        List<Integer> list = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
+
+        List<String> mappedList = mapList(list, new Lambda<String, List<Integer>>() {
+            @Override
+            public String lambda(List<Integer> tail) {
+                int sum = 0;
+
+                for (int n : tail) {
+                    sum += n;
+                }
+
+                return Integer.toString(sum);
+            }
+        });
+
+        assertNotNull("mapped list created", mappedList);
+        assertNotSame("mapped list different", list, mappedList);
+        assertEquals("mapped list size correct", list.size(), mappedList.size());
+        assertEquals("mapped elemet 0 correct", "10", mappedList.get(0));
+        assertEquals("mapped elemet 1 correct", "9", mappedList.get(1));
+        assertEquals("mapped elemet 2 correct", "7", mappedList.get(2));
+        assertEquals("mapped elemet 3 correct", "4", mappedList.get(3));
     }
 }
